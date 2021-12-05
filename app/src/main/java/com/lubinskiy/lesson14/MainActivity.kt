c32ce1c7c9124f884d8928afffa9b5b1b657cf8a
Dmitriy Lubinskiy <dimka00788@gmail.com>
2021-12-05 14:49:41 +0300

    Changed styles & complete MainActivity-layout

--- app/src/main/java/com/lubinskiy/lesson14/MainActivity.kt
+++ app/src/main/java/com/lubinskiy/lesson14/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.lubinskiy.lesson14
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickText(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onClickMagicBall(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onClickRiddles(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onClickCalculator(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
