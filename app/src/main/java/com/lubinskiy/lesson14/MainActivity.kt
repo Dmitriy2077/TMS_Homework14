2b4219996e00ff299c96db492662c377a748551a
Dmitriy Lubinskiy <dimka00788@gmail.com>
2021-12-05 18:16:54 +0300

    Complete MagicBallActivity + layout

--- app/src/main/java/com/lubinskiy/lesson14/MainActivity.kt
+++ app/src/main/java/com/lubinskiy/lesson14/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.lubinskiy.lesson14.modules.MagicBallActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,22 +12,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun onClickText(view: View) {
+    fun onClickText(view: View?) {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
 
-    fun onClickMagicBall(view: View) {
+    fun onClickMagicBall(view: View?) {
+        val intent = Intent(this, MagicBallActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onClickRiddles(view: View?) {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
 
-    fun onClickRiddles(view: View) {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun onClickCalculator(view: View) {
+    fun onClickCalculator(view: View?) {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
