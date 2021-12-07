62e814ab37a4fa8a385dd81126d09e7fe113f3c3
Dmitriy Lubinskiy <dimka00788@gmail.com>
2021-12-07 21:34:00 +0300

    Complete RiddlesActivity + layout

--- app/src/main/java/com/lubinskiy/lesson14/MainActivity.kt
+++ app/src/main/java/com/lubinskiy/lesson14/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.lubinskiy.lesson14.modules.RiddlesActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +22,8 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    fun onClickRiddles(view: View) {
-        val intent = Intent(this, MainActivity::class.java)
+    fun onClickRiddles(view: View?) {
+        val intent = Intent(this, RiddlesActivity::class.java)
         startActivity(intent)
     }
 
